bb7a5f6b89133071eb3577f5181d86cac6193cc6
mikeyy2 <adhikari.shrish1@gmail.com>
2022-11-09 19:04:36 +0545

    Fixed url path error in users.py file

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint('users', __name__, url_prefix="/users")
 
-@users_blueprint.route('/users/ping', methods=['GET'])
+@users_blueprint.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify(
         {
